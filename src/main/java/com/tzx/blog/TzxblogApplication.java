6e919f49e9334b1c9c52c9d74d9bc2980eb1c349
tuzongxun <1160569243@qq.com>
2020-01-30 17:07:13 +0800

    项目重构初始化，增加需求和立项说明文档

--- src/main/java/com/tzx/blog/TzxblogApplication.java
+++ src/main/java/com/tzx/blog/TzxblogApplication.java
@@ -2,10 +2,8 @@ package com.tzx.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
 public class TzxblogApplication {
 
 	public static void main(String[] args) {
